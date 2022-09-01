b9d4b46a85bac73323c9d77898779c7b22c377e9
Collinnn <51782585+Collinnn@users.noreply.github.com>
2022-09-01 08:59:57 +0200

    Push

---
+++ configuration.h
@@ -0,0 +1,9 @@
+#ifndef CONFIGURATIOM_H
+#define CONFIGURATIOM_H
+
+#include <string>
+
+std::string const models_path = "C:/Users/Tobias Collin/Documents/GitHub/Intro-rendering/models/";
+
+//CONFIGURATIOM_H
+#endif
