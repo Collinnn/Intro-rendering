b32d1a6441ec38b9a6d692bb626999c4eb7283fb
Collinnn <51782585+Collinnn@users.noreply.github.com>
2022-11-09 14:20:56 +0100

    Caustic + HDR image

--- raytrace/RenderEngine.cpp
+++ raytrace/RenderEngine.cpp
@@ -61,7 +61,7 @@ RenderEngine::RenderEngine()
     use_default_light(true),                                 // Choose whether to use the default light or not
     shadows_on(true),
     background(optix::make_float3(0.1f, 0.3f, 0.6f)),        // Background color
-    bgtex_filename(""),                                      // Background texture file name
+    bgtex_filename("../models/pedestrian_overpass_4k.hdr"),                                      // Background texture file name
     current_shader(0),
     lambertian(scene.get_lights()),
     photon_caustics(&tracer, scene.get_lights(), 1.0f, 50),  // Max distance and number of photons to search for
