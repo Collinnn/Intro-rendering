401e0afe958ac6a3811513e55d610082a8addae4
Collinnn <51782585+Collinnn@users.noreply.github.com>
2022-11-13 17:39:28 +0100

    Update fresnel.h

--- raytrace/fresnel.h
+++ raytrace/fresnel.h
@@ -7,20 +7,27 @@
 
 inline float fresnel_r_s(float cos_theta1, float cos_theta2, float ior1, float ior2)
 {
-  // Compute the perpendicularly polarized component of the Fresnel reflectance
-  return 0.0;
+	float nthetai = ior1 * cos_theta1;
+	float nthetat = ior2 * cos_theta2;
+	// Compute the perpendicularly polarized component of the Fresnel reflectance
+	return (nthetai-nthetat)/(nthetai+nthetat);
 }
 
 inline float fresnel_r_p(float cos_theta1, float cos_theta2, float ior1, float ior2)
 {
-  // Compute the parallelly polarized component of the Fresnel reflectance
-  return 0.0;
+	float nTthetaI = ior1 * cos_theta2;
+	float nIthetaT = ior2 * cos_theta1;
+
+	// Compute the parallelly polarized component of the Fresnel reflectance
+	return (nTthetaI-nIthetaT)/(nTthetaI+nIthetaT);
 }
 
 inline float fresnel_R(float cos_theta1, float cos_theta2, float ior1, float ior2)
 {
   // Compute the Fresnel reflectance using fresnel_r_s(...) and fresnel_r_p(...)
-  return 0.0;
+	
+  return 0.5 * (fresnel_r_p(cos_theta1, cos_theta2, ior1, ior2) * fresnel_r_p(cos_theta1, cos_theta2, ior1, ior2))
+	  + (fresnel_r_s(cos_theta1, cos_theta2, ior1, ior2) * fresnel_r_s(cos_theta1, cos_theta2, ior1, ior2));
 }
 
 #endif // FRESNEL_H
